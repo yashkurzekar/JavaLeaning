6b561b040e273ae307be7c7bb26f87ec94325164
yash kurzekar <yashkurzekar2@gmail.com>
2022-05-15 14:56:45 +0530

    Input in Java

--- src/com/company/InputFromUser.java
+++ src/com/company/InputFromUser.java
@@ -11,7 +11,7 @@ int a = S.nextInt();  //(Method to read from the keyboard)*/
 public class InputFromUser {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter Your Name");
+        System.out.println("Enter Your Name ");
         String name = sc.nextLine();
         System.out.println("Enter Your Age");
         byte age = sc.nextByte();
