fa2d969292ce2b8d800b433058e2cd56f756a03a
yash kurzekar <yashkurzekar2@gmail.com>
2022-05-15 13:36:46 +0530

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello world!");
-        System.out.println("My Name is Yash Kurzekar");
+        System.out.println("My Name is Yash Kurze kar");
     }
 }
