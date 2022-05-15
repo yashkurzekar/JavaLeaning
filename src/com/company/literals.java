3976c59d836f1e52b9e55f57cc49bfca45a10b85
yash kurzekar <yashkurzekar2@gmail.com>
2022-05-15 14:14:13 +0530

    Literals in Java

---
+++ src/com/company/literals.java
@@ -0,0 +1,36 @@
+package com.company;
+//Literals in Java
+/* Literals
+A constant value that can be assigned to the variable is called a literal.
+
+101 – Integer literal
+10.1f – float literal
+10.1 – double literal (default type for decimals)
+‘A’ – character literal
+true – Boolean literal
+“Harry” – String literal
+Keywords
+Words that are reserved and used by the Java compiler. They cannot be used as an Identifier.
+ */
+public class literals {
+    public static void main(String[] args) {
+        byte age = 34;
+        char a = 'Y';
+        float f1 = 6.5f;
+        double d1 = 4.6d;
+        long number = 55455454554545454L;
+        short age2 = 12;
+        boolean c = true;
+        String name = "Yash Kurzekar";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(a);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(age2);
+        System.out.println(c);
+        System.out.println("Profile Ended");
+
+    }
+}
