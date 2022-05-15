b96e33b6d7457f99b3ba0e08cc3273d4e5e39f59
yash kurzekar <yashkurzekar2@gmail.com>
2022-05-15 15:46:27 +0530

    Kilometer ot Miles conversion

---
+++ src/testExam/kilometerToMiles.java
@@ -0,0 +1,17 @@
+package testExam;
+import java.util.Scanner;
+
+//Kilometers to miles
+
+public class kilometerToMiles {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kilometers to Miles convertor");
+        System.out.println("Enter kilometer");
+        float kilometer = sc.nextFloat();
+        float miles = kilometer * 0.621371f;
+        System.out.println(kilometer+" Kilometers means "+miles);
+
+    }
+}
+
