5183c2e144a63f82d462a5d696d148b7e563651f
yash kurzekar <yashkurzekar2@gmail.com>
2022-05-31 18:29:12 +0530

    Employee Details

---
+++ src/testExam/employeeWage.java
@@ -0,0 +1,27 @@
+package testExam;
+import javax.sound.midi.Soundbank;
+import java.util.Scanner;
+
+public class employeeWage {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to Employee Wage");
+        System.out.println("Enter Your name ");
+        String Name = sc.next();
+        System.out.println("Enter Your ID number");
+        int id = sc.nextInt();
+        System.out.println("Enter Your Address");
+        String Add = sc.next();
+        System.out.println("Enter Your Company Name");
+        String company = sc.next();
+
+        System.out.println(Name);
+        System.out.println(id);
+        System.out.println(Add);
+        System.out.println(company);
+
+
+    }
+}
