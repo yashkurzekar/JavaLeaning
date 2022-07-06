8c465d2701901cd047cfa37bede39d1485b85334
yash kurzekar <yashkurzekar2@gmail.com>
2022-07-06 16:24:21 +0530

    String Methiods

--- src/com/company/stringMethods.java
+++ src/com/company/stringMethods.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class stringMethods {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
+        String name = "harry";
 
         System.out.println(name);
         int value =name.length();
@@ -23,5 +23,31 @@ public class stringMethods {
 
         //return new string with index no
         System.out.println(name.substring(3));
+        System.out.println(name.substring(2,3));
+
+        //replace char
+        System.out.println(name.replace('r','p'));
+        //replace string
+        System.out.println(name.replace("rry","ier"));
+
+        //starts with
+        System.out.println(name.startsWith("Har"));
+        //ends with
+        System.out.println(name.endsWith("rry"));
+
+        // what is the character at
+        System.out.println(name.charAt(2));
+
+        //what is the index of a given substring
+        System.out.println(name.lastIndexOf("r"));
+
+        //last index of
+        String name3 = "Harryrryrry";
+        System.out.println(name3.lastIndexOf("rry",4));
+
+        // Equals
+        System.out.println(name.equals("harry"));
+
+        System.out.println(name.equalsIgnoreCase("HaRrY"));
     }
 }
