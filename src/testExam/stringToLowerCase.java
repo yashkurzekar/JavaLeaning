5a3eba990e7ff572fd589e51518954ee0f96dd10
yash kurzekar <yashkurzekar2@gmail.com>
2022-07-08 14:46:07 +0530

    convert whole string to lowercase

---
+++ src/testExam/stringToLowerCase.java
@@ -0,0 +1,17 @@
+package testExam;
+import java.util.Scanner;
+
+public class stringToLowerCase {
+
+    public static void main(String[] args) {
+        System.out.println("Enter the string ");
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line.toUpperCase());
+
+
+
+
+
+    }
+}
