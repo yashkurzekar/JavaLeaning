74120118a9e4957cd005c10f898e66fca1e193ec
yash kurzekar <yashkurzekar2@gmail.com>
2022-07-08 14:44:28 +0530

    conditionals

---
+++ src/com/company/conditionals.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class conditionals {
+    public static void main(String[] args) {
+        int age = 28;
+        if(age>18) {
+            System.out.println("Yes boy you can drive");
+        }
+        else{
+            System.out.println("You cannot dive ");
+        }
+    }
+}
